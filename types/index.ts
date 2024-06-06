504924f55375a2b1071e7237cd96ca5f1b920689
mrbonk97 <mrbonk97@gmail.com>
2024-06-06 19:07:07 +0900

    타입 기능 추가

--- types/index.ts
+++ types/index.ts
@@ -7,3 +7,34 @@ export type workType = {
   name: string;
   department: string;
 };
+
+export type linkType = {
+  order: number;
+  desc: string;
+  url: string;
+};
+
+export type languageType = {
+  order: number;
+  language: string;
+  test: string;
+  score: string;
+};
+
+export type certiType = {
+  order: number;
+  date: string;
+  title: string;
+  desc: string;
+};
+
+export type educationType = {
+  order: number;
+  startYear: string;
+  startMonth: string;
+  endYear: string;
+  endMonth: string;
+  isWorking: boolean;
+  school: string;
+  major: string;
+};
