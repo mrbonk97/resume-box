ba2c7f15d2e9dc22e4e5b09ab22224475cd54ed6
mrbonk97 <mrbonk97@gmail.com>
2024-06-07 00:50:52 +0900

    feat 페이지 프로그레스 바 추가

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { Sidenav } from "@/components/nav/side-nav";
-import { Input } from "@/components/ui/input";
 import { CardWork } from "@/components/card/card-work";
-import { createRef, useEffect, useRef } from "react";
+import { createRef, useEffect, useRef, useState } from "react";
 import { SaveNavbar } from "@/components/nav/floating-nav";
 import { CardProfile } from "@/components/card/card-profile";
 import { CardLink } from "@/components/card/card-link";
@@ -10,6 +9,7 @@ import { CardLanguage } from "@/components/card/card-language";
 import { CardCertificate } from "@/components/card/card-certi";
 import { CardEducation } from "@/components/card/card-education";
 import { Topnav } from "@/components/nav/top-nav";
+import { PageProgress } from "@/components/page-progress";
 
 const CreatePage = () => {
   const profileRef = useRef<HTMLDivElement>(null);
@@ -20,14 +20,12 @@ const CreatePage = () => {
   const linkRef = useRef<HTMLDivElement>(null);
   const coverRef = useRef<HTMLDivElement>(null);
 
+  const mainRef = useRef<HTMLDivElement>(null);
+
   const handleScroll = (type: string) => {
     if (type == "PROFILE")
       profileRef.current?.scrollIntoView({ behavior: "smooth" });
-    if (type == "WORK") {
-      console.log(workRef.current);
-
-      workRef.current?.scrollIntoView({ behavior: "smooth" });
-    }
+    if (type == "WORK") workRef.current?.scrollIntoView({ behavior: "smooth" });
     if (type == "EDUCATION")
       eduRef.current?.scrollIntoView({ behavior: "smooth" });
     if (type == "CERTIFICATE")
@@ -39,10 +37,19 @@ const CreatePage = () => {
       coverRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  if (!isLoaded) return null;
+
   return (
     <>
+      <PageProgress mainRef={mainRef} />
       <Topnav />
-      <main className="min-h-full w-full bg-slate-200">
+      <main className="min-h-full w-full bg-slate-200" ref={mainRef}>
         <Sidenav handleScroll={handleScroll} />
         <div className="pl-[400px] pt-20 pr-10 w-full space-y-10">
           <CardProfile ref={profileRef} />
@@ -51,7 +58,7 @@ const CreatePage = () => {
           <CardCertificate ref={certieRef} />
           <CardLanguage ref={langRef} />
           <CardLink ref={linkRef} />
-          <div className="h-[600px] w-full">asd</div>
+          <div className="h-[600px] w-full"></div>
           <div className="h-[600px] w-full" />
         </div>
         <SaveNavbar />
